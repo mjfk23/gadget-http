d89cc16987e0cc3f7b88433d30b1ed48ed931ca2
Melody Kimball <melody@forestkimball.com>
2024-11-07 15:48:35 -0500

    Added interface

--- src/Exception/HttpException.php
+++ src/Exception/HttpException.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Gadget\Http\Exception;
 
 use Gadget\Lang\Exception;
+use Psr\Http\Client\ClientExceptionInterface;
 
-final class HttpException extends Exception
+final class HttpException extends Exception implements ClientExceptionInterface
 {
 }
