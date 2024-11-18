e2f9b19be80d266d86b2a32e03d9a12cb7d489dc
Melody Kimball <melody@forestkimball.com>
2024-11-18 08:04:14 -0500

    Added some common content types

--- src/Message/ContentType.php
+++ src/Message/ContentType.php
@@ -6,12 +6,19 @@ namespace Gadget\Http\Message;
 
 enum ContentType : string
 {
-    case CSS = 'text/css';
-    case FORM = 'application/x-www-form-urlencoded';
-    case HTML = 'text/html';
-    case JAVASCRIPT = 'text/javascript';
+    case GZIP = 'application/gzip';
     case JSON = 'application/json';
     case STREAM = 'application/octet-stream';
-    case TEXT = 'text/plain';
+    case PDF = 'application/pdf';
+    case DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+    case FORM = 'application/x-www-form-urlencoded';
+    case XLSX = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
     case XML = 'application/xml';
+    case ZIP = 'application/zip';
+
+    case CSS = 'text/css';
+    case CSV = 'text/csv';
+    case HTML = 'text/html';
+    case JAVASCRIPT = 'text/javascript';
+    case TEXT = 'text/plain';
 }
