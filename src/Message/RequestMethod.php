10ea68c5c552283d8ac84876e480913237db3d93
Melody Kimball <melody@forestkimball.com>
2024-11-18 08:04:28 -0500

    Added all HTTP request methods

--- src/Message/RequestMethod.php
+++ src/Message/RequestMethod.php
@@ -7,7 +7,12 @@ namespace Gadget\Http\Message;
 enum RequestMethod : string
 {
     case GET = 'GET';
+    case HEAD = 'HEAD';
     case PUT = 'PUT';
     case POST = 'POST';
     case DELETE = 'DELETE';
+    case CONNECT = 'CONNECT';
+    case OPTIONS = 'OPTIONS';
+    case TRACE = 'TRACE';
+    case PATCH = 'PATCH';
 }
